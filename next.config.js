233f702dc54ee9d69443f774950abaab1d8a6466
ArturBortniczuk <a.bortniczuk@grupaeltron.pl>
2025-06-13 12:07:49 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,8 +3,17 @@ const nextConfig = {
   images: {
     domains: ['localhost'],
   },
-  // Wyłącz React Strict Mode który może powodować problemy z hydration
+  // Wyłącz React Strict Mode
   reactStrictMode: false,
+  
+  // Wyłącz SWC minifier który może powodować problemy
+  swcMinify: false,
+  
+  // Dodaj experimental flags dla lepszej stabilności
+  experimental: {
+    suppressHydrationWarning: true
+  },
+  
   async redirects() {
     return [
       {
